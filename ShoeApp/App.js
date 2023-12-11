063737db92a84d446279088316e7da8762df6ab4
demonCthao <congthao1103@gmail.com>
2023-12-12 01:49:37 +0700

    commit2
    cmit two :3

--- ShoeApp/App.js
+++ ShoeApp/App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import SplashScreen from './component/SplashScreen'
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRoutName={'SplashScreen'}>
+          <Stack.Screen name='SplashScreen' component={SplashScreen} options={{headerShown:false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
---
+++ ShoeApp/component/Home.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View,ScrollView, Image, TouchableOpacity } from 'react-native';
+
+function Home({navigation}){
+
+    return(
+        <View style>
+            
+        </View>
+    )
+}
+export default Home;
+const styles = StyleSheet.create({
+    
+})
